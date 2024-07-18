e5079e3feb89c2693565876cd62b9103d0df65da
Камила <105364337+Camihak@users.noreply.github.com>
2024-07-18 12:10:48 +0300

    Создает компоненты для всех страниц приложения

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
+
+const Setting = {
+  OffersNumber: 15,
+} as const;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +12,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
-  </React.StrictMode>
+    <App
+      offersNumber = {Setting.OffersNumber}
+    />
+  </React.StrictMode>,
 );
