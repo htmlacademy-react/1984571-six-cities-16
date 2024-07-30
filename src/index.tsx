0586c816723cb95a78e701e20f142b65bbd20b02
Камила <105364337+Camihak@users.noreply.github.com>
2024-07-30 10:43:24 +0300

    Выносит константу в файл consts.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
-const Setting = {
-  OffersNumber: 15,
-} as const;
+import { Setting } from './consts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
